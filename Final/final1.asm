54b83eb0a1dbaa1003a52ba47dd5f14b5c11be2d
Josue-Contreras <51929041+Josue-Contreras@users.noreply.github.com>
2021-12-17 16:44:48 -0800

    Added MM I/O loop

--- Final/final1.asm
+++ Final/final1.asm
@@ -12,11 +12,17 @@ txt2:   .asciiz "The number: "
         .text
         .globl main
 main:
-        li      $t0, 100        # Multiplier $t0 = 100
+        li      $t0, 100                # Multiplier $t0 = 100
 
-        li      $v0, 4          # Print syscall code
-        la	$a0, txt1       # Address of "txt1"
-        syscall                 # Prints "txt1"
+        li      $v0, 4                  # Print syscall code
+        la	$a0, txt1               # Address of "txt1"
+        syscall                         # Prints "txt1"
 
-        
+        lui     $t1, 0xFFFF             # Memory location (Reciever Control) $t1 = 0xFFFF
+rd_wait:
+        lw	$t1, 0($t1)             # Load from reciever control register      
+        andi	$t1, $t1, 1             # Clear all bits of except for LSB
+        beqz    $t1, rd_wait            # If not ready(0), restart, otherwise(1) continue to calculation
+        lw      $t2, 4($t1)             # Read data from reciever data to $t2
+output: 
         

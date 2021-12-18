aa45b6f58a56cbee73390f4927663c9a1e1fabae
Josue-Contreras <51929041+Josue-Contreras@users.noreply.github.com>
2021-12-17 17:01:00 -0800

    Finally able to run, made register mistake, now math error

--- Final/final1.asm
+++ Final/final1.asm
@@ -20,13 +20,13 @@ main:
 
         lui     $t1, 0xFFFF             # Memory location (Reciever Control) $t1 = 0xFFFF
 rd_wait:
-        lw	$t1, 0($t1)             # Load from reciever control register      
-        andi	$t1, $t1, 1             # Clear all bits of except for LSB
-        beqz    $t1, rd_wait            # If not ready(0), restart, otherwise(1) continue to calculation
+        lw	$t4, 0($t1)             # Load from reciever control register      
+        andi	$t4, $t4, 1             # Clear all bits of except for LSB
+        beqz    $t4, rd_wait            # If not ready(0), restart, otherwise(1) continue to calculation
         lw      $t2, 4($t1)             # Read data from reciever data to $t2
 
         sub	$t2, $t2, 48            # Subtract $t2 by 48 convert from ASCII to int 
-        mul	$t2, $t2, $t0           # Multiply $t2 but the multiplyer from $t0
+        mul	$t2, $t2, $t0            # Multiply $t2 but the multiplyer from $t0
         div	$t0, $t0, 10	        # Divide $t0 to decrease place value
         add	$t3, $t3, $t2           # Add current digit values
 
@@ -35,7 +35,7 @@ rd_wait:
         la      $a0, txt2               # Address of the exit number prompt
         syscall                         # Print message
 
-        move    $a0, $t4                # Move output to argument register 
+        move    $a0, $t0                # Move output to argument register 
         li      $v0, 1                  # Print int syscall code
         syscall                         # Print int 
 
